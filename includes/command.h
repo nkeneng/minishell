86e1e488f36f7e7b50ddaf7e56b88e73b6a2ecd6
Lea Meubrink <lmeubrin@c3a4c2.42berlin.de>
2024-10-08 16:47:43 +0200

    changed struct

--- includes/command.h
+++ includes/command.h
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 12:21:58 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/10/02 18:06:44 by stevennke        ###   ########.fr       */
+/*   Updated: 2024/10/08 16:46:32 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,14 @@
 /* UNUSED		(1 << 30)	*/
 
 /* possible values for the `flags' field of command */
-# define C_PIPE	0x01 // needs to read from a pipe
-# define C_HERE_DOC	0x02 // needs to read from a here doc
-# define C_OPEN_INFILE	0x04 
-# define C_OPEN_OUT_TRUNC	0x08
-# define C_OPEN_OUT_APP	0x10
-# define C_LAST_PIPE	0x20
+# define C_EXECUTE	(1 << 1) // needs to read from a pipe
+# define C_COMMAND_BUILTIN 1 << 2
+# define C_PIPE	1 << 3 // needs to read from a pipe
+# define C_HERE_DOC	1 << 4 // needs to read from a here doc
+# define C_OPEN_INFILE	1 << 5 // open file as infile
+# define C_OPEN_OUT_TRUNC	1 << 6 // open output file truncated
+# define C_OPEN_OUT_APP	1 << 7 // open output file for appending
+# define C_LAST_PIPE	1 << 8 // execute command output to stdout
 
 /* Flags for the `pflags' argument to param_expand() and various
    parameter_brace_expand_xxx functions; also used for string_list_dollar_at */
@@ -96,8 +98,10 @@
 
 /* A structure which represents a command. */
 typedef struct t_command {
-  char *cmd;		/* Zero terminated string. */
-  int flags;		/* Flags associated with this command. */
+  char	*cmd;		/* Zero terminated string. */
+  char	*infile;
+  char	*outfile;
+  int	flags;		/* Flags associated with this command. */
 } t_command;
 
 /* A structure which represents a word. */
