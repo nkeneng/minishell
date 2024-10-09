d9b64a731a87371356ad0da24e88b5cbdf564be5
Lea Meubrink <lmeubrin@c3a4c2.42berlin.de>
2024-10-09 14:00:23 +0200

    work in progress

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:09:04 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/10/09 12:28:46 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/10/09 14:00:02 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,73 @@ executes a command. Basically, the shell does the following:
 	(see Exit Status).
 */
 
+t_word_list	*split_into_words(char *line);
+
 t_list	*parse_input(char *line, char **envp)
 {
-	t_list	**list;
+	t_list		*list;
 	t_word_list	*word_list;
 	//optionally open script if strstr(line, ".sh") and read from it
 	word_list = split_into_words(line);
+	return (list);
 }
 
 //Breaks the input into words and operators, obeying the quoting rules 
 //described in Quoting. These tokens are separated by metacharacters. 
 //Alias expansion is performed by this step (see Aliases).
-t_word_list	split_into_words(char *line)
+t_word_list	*split_into_words(char *line)
 {
 	t_word_list	*word_list;
 	t_word_list	*current_word;
-	char *word;
+	char		*word;
+	char		last_special;
+	char		*next_special;
+	int			i;
+	int			start_split;
+
+	word_list = current_word;
+	current_word = NULL;
+	start_split = 0;
+	while (line[i])
+	{
+		next_special = ft_strchr("\"\'$()<>|", line[i]);
+		if (next_special)
+		{
+			if (start_split == i)
+			{
+				last_special = *next_special;
+				continue ;
+			}
+			if (*next)
+			if (last_special == '\'' && last_special == *next_special)
+			if (*next_special == '\'')
+		}
+				
+			
+		i++;
+	}
+	return (word_list);
+}
+
+t_word_list	*seperate_at(char *line, char sign)
+{
+	int	i;
+	int	open;
+
+	i = 0;
+	open = 0;
+	while (line[i])
+	{
+		if (line[i] == sign)
+		{
+			if (open > 0)
+			{
+				open--;
+			}
+			open++;
+		}
+		i++;
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -61,7 +112,7 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		line = rl_gets();
-		if (!missing_close_sign(line, '\''))
+		while (missing_close_sign(line, '\''))
 			ft_strappend(line, rl_gets());
 		args = parse_input(line, envp);
 		num = ft_lstsize(args);
