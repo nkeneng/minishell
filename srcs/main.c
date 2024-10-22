51422528819f9017822bbcccfc4e515a1b937b9d
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-10-22 15:56:12 +0200

    changed env from list to arr

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: stevennkeneng <snkeneng@student.42ber      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 19:13:25 by stevennke         #+#    #+#             */
-/*   Updated: 2024/10/22 12:38:02 by snkeneng         ###   ########.fr       */
+/*   Updated: 2024/10/22 15:53:06 by stevennke        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,28 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_list	*g_envp;
 	t_list	*tmp;
 	t_shell	shell;
+	int		i;
 
 	(void)argc;
 	(void)argv;
 	(void)shell;
 	(void)tmp;
-	(void)g_envp;
-	int i = 0;
-	while (envp[i])
-	{
-		if (envp[i+1] == NULL)
-			ft_printf("%s\n", envp[i]);
-		i++;
-	}
+	(void)i;
+	i = 0;
 	shell.envp = NULL;
-	init_envp(envp, &shell.envp);
-	 ft_export("hello=world", &shell);
-	 ft_export("PATH=world", &shell);
-	tmp = shell.envp;
-	while(tmp)
-	{
-		ft_printf("Key : %s\n Value : %s\n", ((t_env *)tmp->content)->key,
-			((t_env *)tmp->content)->value);
-		tmp = tmp->next;
-	}
+	init_envp(envp, &shell);
+	ft_export("hello=world", &shell);
+	ft_export("PATH=world", &shell);
+	ft_printf("number of env variables: %d\n", shell.nb_env);
+	// tmp = shell.envp;
+	// while (tmp)
+	// {
+	// 	ft_printf("Key : %s\n Value : %s\n", ((t_env *)tmp->content)->key,
+	// 		((t_env *)tmp->content)->value);
+	// 	tmp = tmp->next;
+	// }
 	/* tmp = ft_lstlast(g_envp); */
 	/* ft_printf("Key : %s\n Value : %s\n", ((t_env *)tmp->content)->key, */
 	/* 	((t_env *)tmp->content)->value); */
