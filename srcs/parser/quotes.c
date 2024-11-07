9b486698c460416d8d553edf4d1b2eeb9e68df10
Moat423 <schluesselfehlt@gmail.com>
2024-11-07 17:48:20 +0100

    made quote removal. Todo: expansion

--- srcs/parser/quotes.c
+++ srcs/parser/quotes.c
@@ -6,12 +6,28 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 12:44:56 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/10/25 11:01:01 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:46:06 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+// function removes beginning and ending quotes from word
+// param: word_desc with flag W_SQUOTED or W_DQUOTED
+// returns word_desc
+t_word_desc	*remove_quotes(t_word_desc *word)
+{
+	int	len;
+
+	if (!(word->flags & W_SQUOTED || word->flags & W_DQUOTED))
+		return (word);
+	len = ft_strlen(word->word);
+	word->word[len - 1] = '\0';
+	ft_memmove(word->word, word->word + 1, len - 1);
+	return (word);
+}
+
+// NOT USING THE FOLLOWING TWO FUNCITONS I THINK
 // returns the i that it stopped at, fills to_fill with content from line
 // until it hits a valid delim (|, <, >, "", '', ' ')
 // or until the next one of that kind is hit
