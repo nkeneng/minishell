166459dc43b838b17e74322d9a489b37d5e10cf8
steven nkeneng <nkeneng.junior@yahoo.fr>
2024-11-10 15:07:21 +0000

    working version

--- includes/list.h
+++ includes/list.h
@@ -22,6 +22,6 @@ int		ft_printf_command(void *command);
 t_list	*convert_word_list_to_list(t_word_list *word_list);
 t_list	*make_redirect_list(t_word_list **list);
 t_word_list	*find_redir(t_word_list *list);
-t_command	*make_command_list(t_word_list *word_list, t_word_list *curr);
+t_command	*make_command_list(t_word_list **word_list);
 int	ft_printf_redirect(void *redirect);
 #endif //!LIST_H
--- srcs/lst/ft_make_command_list.c
+++ srcs/lst/ft_make_command_list.c
@@ -17,30 +17,29 @@ int	get_number_of_words_before_pipe(t_word_list *word_list)
 	return (number_of_words_before_pipe);
 }
 
-t_command	*make_command_list(t_word_list *word_list, t_word_list *curr)
+t_command	*make_command_list(t_word_list **word_list)
 {
 	t_command	*command;
 	int			number_of_words_before_pipe;
 	int i = 0;
 	t_word_list	*tmp;
 
-	(void)curr;
+	if(!word_list)
+		return (NULL);
 	number_of_words_before_pipe = 0;
 	command = (t_command *)malloc(sizeof(t_command));
-	number_of_words_before_pipe = get_number_of_words_before_pipe(word_list);
+	number_of_words_before_pipe = get_number_of_words_before_pipe(*word_list);
 	command->cmd = (char **)malloc(sizeof(char *) * (number_of_words_before_pipe
 				+ 1));
-	tmp = word_list;
+	tmp = *word_list;
 	while (i < number_of_words_before_pipe)
 	{
-		ft_printf("word_list->word->word : %s\n", word_list->word->word);
-		command->cmd[i] = ft_strdup(word_list->word->word);
-		word_list_delone(&word_list, tmp);
-		tmp = word_list;
+		command->cmd[i] = ft_strdup((*word_list)->word->word);
+		word_list_delone(word_list, tmp);
+		tmp = *word_list;
 		i++;
 	}
-	word_list_delone(&word_list, tmp); // remove the pipe
+	word_list_delone(word_list, tmp); // remove the pipe
 	command->cmd[i] = NULL;
-	curr = word_list;
 	return (command);
 }
