ca7d2b91e0e51aeb34224eb995746789cb5f086d
Lea Meubrink <lmeubrin@c3a3c7.42berlin.de>
2024-11-19 18:58:13 +0100

    corrected wl_insert_instead and some norminette

--- srcs/parser/make_word_list.c
+++ srcs/parser/make_word_list.c
@@ -6,11 +6,13 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 13:31:07 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/11/18 15:59:53 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/11/19 17:32:36 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+// TODO: segfaults on split_around when just \n is entered
 
 // remove_whitespace_element and sets flags for previous item to have had space
 t_word_list	*remove_whitespace_element(t_word_list **head, t_word_list *curr)
@@ -61,6 +63,8 @@ t_word_list	*split_element_at_wh(t_word_list **word_list, t_word_list *item)
 		if (!tmp)
 			return (free_word_list(word_list));
 	}
+	else
+		tmp = item;
 	return (tmp);
 }
 
@@ -68,32 +72,60 @@ int	loop_to_split_on_spaces(t_word_list **word_list, t_shell *shell)
 {
 	t_word_list	*curr;
 	t_word_list	*tmp;
+	t_word_list	*first;
 
 	(void)shell;
 	curr = *word_list;
 	while (curr)
 	{
-		tmp = curr;
-		curr = split_element_at_wh(word_list, curr);
+		ft_printf("\nProcessing current word: %s\n", curr->word->word);
+		tmp = split_element_at_wh(word_list, curr);
 		if (!tmp)
+		{
+            ft_printf("split_element_at_wh returned NULL\n");
 			free_word_list(word_list);
-		ft_printf("\nWORD in LOOP:\n");
-		ft_printf_word_desc(curr->word);
-		if (!(curr->word->flags & W_EXPANDED))
-			curr = expand_and_split(word_list, curr, shell);
+			return (1);
+		}
+        ft_printf("After split - tmp points to: %s\n", tmp->word->word);
+        ft_printf("Current list state:\n");
+        ft_printf_word_list(*word_list);
+		if (!(tmp->word->flags & W_EXPANDED))
+		{
+			curr = expand_and_split(word_list, first, shell);
+			if (!curr)
+			{
+				free_word_list(word_list);
+				return (1);
+			}
+			ft_printf("After expansion - curr points to: %s\n", curr->word->word);
+			ft_printf_word_desc(curr->word);
+		}
+		else
+			curr = tmp;
 		curr = remove_whitespace_element(word_list, curr);
-		ft_printf("\nWORD after expansion:\n");
-		ft_printf_word_desc(curr->word);
-		if (!tmp)
+		if (!curr)
+		{
+            ft_printf("remove_whitespace_element returned NULL\n");
 			free_word_list(word_list);
+			return (1);
+		}
+		ft_printf("Before moving to next - curr points to: %s\n", curr->word->word);
+        if (curr->next)
+            ft_printf("Next word will be: %s\n", curr->next->word->word);
+        else
+            ft_printf("No next word\n");
 		curr = curr->next;
+		first = curr;
 	}
+    ft_printf("\nWORD LIST at the end of LOOP:\n");
+    ft_printf_word_list(*word_list);
 	return (0);
 }
 
 t_word_list	*expand_and_split(t_word_list **word_list, t_word_list *curr, t_shell *shell)
 {
 	int			expand_ret;
+	t_word_list	*tmp;
 
 	expand_ret = ft_expand_variable_name(curr->word, shell);
 	if (expand_ret == 0)
@@ -102,8 +134,13 @@ t_word_list	*expand_and_split(t_word_list **word_list, t_word_list *curr, t_shel
 		return (NULL);
 	}
 	else if (expand_ret == -2)
-		curr = split_element_at_wh(word_list, curr);
-	return (curr);
+		tmp = split_element_at_wh(word_list, curr);
+	else
+	{
+		curr->word->flags |= W_EXPANDED;
+		tmp = curr;
+	}
+	return (tmp);
 }
 
 int	loop_on_word_list(t_word_list **word_list)
