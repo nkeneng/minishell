eb2a113fad984b79fbf9e6e7b4dc69f2298a9c73
Lea Meubrink <lmeubrin@c4b8c2.42berlin.de>
2024-11-20 13:23:47 +0100

    added file containing the loops for expansion

---
+++ srcs/parser/wl_expansion.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wl_expansion.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 11:06:21 by lmeubrin          #+#    #+#             */
+/*   Updated: 2024/11/20 12:57:28 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+//function returns last element of list that was inserted instead of word,
+//if curr was expanded
+//or curr if not expanded
+//or next element if curr was deleted because it got expanded to empty string
+t_word_list	*expand_and_split(t_word_list **word_list, t_word_list *curr, t_shell *shell)
+{
+	int			expand_ret;
+
+	expand_ret = ft_expand_variable_name(curr->word, shell);
+	if (expand_ret == 0)
+	{
+		free_word_list(word_list);
+		return (NULL);
+	}
+	else if (expand_ret == -2)
+		curr = split_element_at_wh(word_list, curr);
+	else if (expand_ret == -3)
+		curr = wl_delone(word_list, curr);
+	if (!curr)
+		return (NULL);
+	return (curr);
+}
+
+// expands all variables in the word_list
+// if a valiable is at the end at expanded to empty string it returns 2
+// if a malloc error occurs it returns 1
+// else returns 0
+int	wl_expand_list(t_word_list **word_list, t_shell *shell)
+{
+	t_word_list	*curr;
+
+	curr = *word_list;
+	while (curr)
+	{
+		if (!(curr->word->flags & W_EXPANDED))
+			curr = expand_and_split(word_list, curr, shell);
+		if (!curr)
+			return (2);
+		curr = wl_remove_whitespace_element(word_list, curr);
+		if (!curr)
+		{
+			free_word_list(word_list);
+			return (1);
+		}
+		curr = curr->next;
+	}
+	return (0);
+}
