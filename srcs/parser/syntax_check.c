d4eccf417615da7c1e3612226cbf28c2d0ecf622
Lea Meubrink <lmeubrin@c4b8c2.42berlin.de>
2024-11-22 13:39:47 +0100

    general expansion should be working

--- srcs/parser/syntax_check.c
+++ srcs/parser/syntax_check.c
@@ -6,12 +6,17 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 13:30:47 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/11/20 14:27:14 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/11/22 10:19:43 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+// performs syntax checks on list:
+// 1. checks if operator is followed by another operator or newline
+// 2. checks if redirection operator is followed by another redirection operator
+// or operator or newline
+// returns 0 if no syntax errors found
 int	wl_syntax_check(t_word_list **head)
 {
 	t_word_list	*curr;
@@ -19,14 +24,21 @@ int	wl_syntax_check(t_word_list **head)
 	curr = *head;
 	while (curr)
 	{
-		if (curr->word->flags & W_EXPANDED)
+		if (curr->word->flags & WM_OPERATOR_MASK)
 		{
-			identify_word_type(curr->word);
-			if (curr->word->flags & (WM_REDIR_MASK | WM_OPERATOR_MASK))
-			{
-				if (!(curr->next))
-					syntax_error(head, curr->word->word);
-			}
+			if (!(curr->next))
+				syntax_error(head, "newline");
+			if (curr->next->word->flags & WM_OPERATOR_MASK)
+				syntax_error(head, curr->next->word->word);
+		}
+		if (curr->word->flags & ~W_EXPANDED && \
+			WM_REDIR_MASK & identify_word_type(curr->word))
+		{
+			if (!(curr->next))
+				syntax_error(head, "newline");
+			if (curr->next->word->flags & WM_OPERATOR_MASK && \
+			WM_REDIR_MASK & identify_word_type(curr->next->word))
+				syntax_error(head, curr->next->word->word);
 		}
 		curr = curr->next;
 	}
--- srcs/parser/syntax_error.c
+++ srcs/parser/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:04:39 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/11/20 13:59:23 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/11/22 10:34:34 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ int	syntax_error(t_word_list **word_list, char *str)
 {
 	errno = 2;
 	ft_fprintf(2, "minishell: syntax error near unexpected token `%c'\n", *str);
+	free_word_list(word_list);
+	return (errno);
+}
+
+int	syntax_error_unexpected_eof(t_word_list **word_list)
+{
+	errno = 2;
+	ft_fprintf(2, "minishell: syntax error: unexpected end of file\n");
 	free_word_list(word_list);
 	return (errno);
 }
