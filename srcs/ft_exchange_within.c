a28443ab174533dedd2b64fdc0277a98738f627c
Lea Meubrink <lmeubrin@student.42berlin.de>
2024-12-01 12:35:59 +0100

    small cleanup and norminetting

--- srcs/ft_exchange_within.c
+++ srcs/ft_exchange_within.c
@@ -1,4 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exchange_within.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 12:31:16 by lmeubrin          #+#    #+#             */
+/*   Updated: 2024/12/01 12:31:29 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "../../includes/minishell.h"
 
 // function that exchanges a substring within a string
 // from is the index of the first char of the substring to exchange
