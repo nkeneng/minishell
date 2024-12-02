78cab481601b533d66e489a108450fb69e2d495d
Steven Nkeneng <47323934+nkeneng@users.noreply.github.com>
2024-12-02 18:47:04 +0100

    Feature/redirections (#47)
    * redirection output in between command working
    
    * handled input file redirection for middles commands
    
    * handled redirection for last and single commands
    
    * unused comment

--- srcs/pipex/command.c
+++ srcs/pipex/command.c
@@ -31,29 +31,39 @@ char **env_to_array(t_env *envp)
 	envp_array[i] = NULL;
 	return (envp_array);
 }
-
-int handle_builtin(t_command *command, t_env **envp)
+static int has_output_redirection(t_list *redirects)
 {
-	if (ft_strncmp(command->cmd[0], "cd", ft_strlen("cd")) == 0)
-		return(ft_cd(command->cmd[1]));
- 	else if (ft_strncmp(command->cmd[0], "echo", ft_strlen("echo")) == 0)
+	while (redirects && redirects->content)
 	{
-		if (ft_strncmp(command->cmd[1], "-n", ft_strlen("-n")) == 0)
-			return(ft_echo(command->cmd, 1));
-		else
-			return(ft_echo(command->cmd, 0));
+		t_redirect *redir = redirects->content;
+		if (redir->filename->flags & (W_OPEN_OUT_TRUNC | W_OPEN_OUT_APP))
+			return (1);
+		redirects = redirects->next;
 	}
-	else if (ft_strncmp(command->cmd[0], "pwd", ft_strlen("pwd")) == 0)
-		return(ft_pwd());
-	else if (ft_strncmp(command->cmd[0], "export", ft_strlen("export")) == 0)
-		return(ft_export(command->cmd, envp));
-	else if (ft_strncmp(command->cmd[0], "unset", ft_strlen("unset")) == 0)
-		return (ft_unset(command->cmd, envp));
-	else if (ft_strncmp(command->cmd[0], "env", ft_strlen("env")) == 0)
-		return (ft_env(env_to_array(*envp)));
-	else if (ft_strncmp(command->cmd[0], "exit", ft_strlen("exit")) == 0)
-		ft_exit();
-	return (EXIT_SUCCESS);
+	return (0);
+}
+
+static void handle_redirections(t_command *cmd)
+{
+    t_list *redir_list;
+    t_redirect *redir;
+
+    redir_list = cmd->redirects;
+    while (redir_list && redir_list->content)
+    {
+        redir = redir_list->content;
+        if (redir->filename->flags & (C_OPEN_INFILE))
+        {
+            ft_printf("opening input file %s\n", redir->filename->word);
+            open_doc(redir->filename->word, redir->filename->flags);
+        }
+        else if (redir->filename->flags & (W_OPEN_OUT_TRUNC | W_OPEN_OUT_APP))
+        {
+            ft_printf("opening output file %s\n", redir->filename->word);
+            open_doc(redir->filename->word, redir->filename->flags);
+        }
+        redir_list = redir_list->next;
+    }
 }
 
 int	exec_builtin(int builtin, t_command *command, t_env **envp)
@@ -80,39 +90,15 @@ int	exec_builtin(int builtin, t_command *command, t_env **envp)
 	return (0);
 }
 
-int	exec_command(t_command *command, t_env **envp, int *fd)
-{
-	pid_t	cpid;
-	char	**envp_array;
-
-	cpid = fork();
-	if (cpid == -1)
-		return (rperror("fork"));
-	else if (cpid == 0)
-	{
-		init_signals_noninteractive();
-		close(fd[0]);
-		if (dup2(fd[1], STDOUT_FILENO) == -1)
-			return (rperror("dup2"));
-		close(fd[1]);
-		if (command->flags & C_BUILTIN)
-			exit(exec_builtin(is_builtin(command->cmd[0]), command, envp));
-		else
-		{
-			envp_array = env_to_array(*envp);
-			make_exec(command, envp_array);
-		}
-		exit(errno);
-	}
-	return (cpid);
-}
-
+//@TODO: handle redirection not working with builtins ( because of how it outputs ? )
+//@TODO: handle heredoc not working yet
 int	pipex(t_env **envp, t_list **cmd_list)
 {
 	int		pipefd[2];
 	pid_t	cpid;
 	t_list	*tmp_list;
 	int		i;
+	t_command *cmd;
 
 	tmp_list = *cmd_list;
 	i = 0;
@@ -121,9 +107,29 @@ int	pipex(t_env **envp, t_list **cmd_list)
 	{
 		if (pipe(pipefd) == -1)
 			return (rperror("pipe"));
-		cpid = exec_command(tmp_list->content, envp, pipefd);
+		cpid = fork();
 		if (cpid == -1)
 			return (rperror("fork"));
+		else if (cpid == 0)
+		{
+			init_signals_noninteractive();
+			cmd = (t_command *)tmp_list->content;
+			handle_redirections(cmd);
+			if (!has_output_redirection(cmd->redirects))
+			{
+				if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+					exit(rperror("dup2"));
+			}
+			close(pipefd[1]);
+			if (cmd->flags & C_BUILTIN)
+				exit(exec_builtin(is_builtin(cmd->cmd[0]), cmd, envp));
+			else
+			{
+				char **envp_array = env_to_array(*envp);
+				make_exec(cmd, envp_array);
+			}
+			exit(errno);
+		}
 		close(pipefd[1]);
 		if (dup2(pipefd[0], STDIN_FILENO) == -1)
 			return (rperror("dup2"));
@@ -131,38 +137,42 @@ int	pipex(t_env **envp, t_list **cmd_list)
 		tmp_list = tmp_list->next;
 		i++;
 	}
-	close(pipefd[0]);
 	return (exec_to_stdout(envp, ft_lstlast(*cmd_list)->content, i));
 }
 
-int	exec_to_stdout(t_env **envp, t_command *cmd, int chld_nb)
+int exec_to_stdout(t_env **envp, t_command *cmd, int chld_nb)
 {
-	pid_t	cpid;
-	int		status;
-	char	**envp_array;
-	int		builtin_nb;
+    pid_t   cpid;
+    int     status;
+    char    **envp_array;
+    int     builtin_nb;
 
-	builtin_nb = is_builtin(cmd->cmd[0]);
-	if (chld_nb == 0 && builtin_nb)
-		return (exec_builtin(builtin_nb, cmd, envp));
-	cpid = fork();
-	if (cpid == -1)
-		return (rperror("fork"));
-	else if (cpid == 0)
-	{
+    builtin_nb = is_builtin(cmd->cmd[0]);
+    if (chld_nb == 0 && builtin_nb)
+    {
+        handle_redirections(cmd);  // Handle redirections for builtin
+        return (exec_builtin(builtin_nb, cmd, envp));
+    }
+    cpid = fork();
+    if (cpid == -1)
+        return (rperror("fork"));
+    else if (cpid == 0)
+    {
 		init_signals_noninteractive();
-		if (builtin_nb)
-			exit(exec_builtin(builtin_nb, cmd, envp)); // using the return value of exec_builtin as return because i don't know how you are handling the returns of the builtins.
-		envp_array = env_to_array(*envp);
-		if (!envp_array)
-			exit(EXIT_FAILURE); // protect all allocations!
-		make_exec(cmd, envp_array); // this shouldn't return, if it does, it's an error.
-		exit(rperror("execve"));
-	}
-	waitpid(cpid, &status, 0);
-	while (chld_nb--)
-		waitpid(-1, NULL, 0);  //this is equal to wait(NULL);
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
-	return (status);
+        handle_redirections(cmd);  // Handle redirections for child process
+        if (builtin_nb)
+            exit(exec_builtin(builtin_nb, cmd, envp));
+        envp_array = env_to_array(*envp);
+        if (!envp_array)
+            exit(EXIT_FAILURE);
+        make_exec(cmd, envp_array);
+        perror("execve");
+        exit(errno);
+    }
+    waitpid(cpid, &status, 0);
+    while (chld_nb--)
+        waitpid(-1, NULL, 0);
+    if (WIFEXITED(status))
+        return (WEXITSTATUS(status));
+    return (status);
 }
