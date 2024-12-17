30e0686826aae1d167e1c051248beb7741099ed5
Moat423 <56015062+Moat423@users.noreply.github.com>
2024-12-17 20:23:27 +0100

    norminette changes and valgrind supp added (#64)

--- srcs/parser/ft_strexchange.c
+++ srcs/parser/ft_strexchange.c
@@ -6,13 +6,14 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:50:39 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/12/12 17:18:33 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/12/17 20:08:31 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-//function to exchange the string to_replace and its preceeding $ with new_string
+//function to exchange the string to_replace and its preceeding $
+// with new_string
 // in the original string and frees the original
 // returns a malloced string or NULL if allocation failed
 char	*ft_strexchange(char *original, int len_old, char *new_str)
