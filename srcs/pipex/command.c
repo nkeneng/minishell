d0dc154c798407225dd20d71293ebf9fc3309fc6
steven nkeneng <nkeneng.junior@yahoo.fr>
2024-12-19 15:33:27 +0000

    refactor pipex and exec_to_stdout functions for improved signal handling and code clarity

--- srcs/pipex/command.c
+++ srcs/pipex/command.c
@@ -51,60 +51,6 @@ int	exec_builtin(int builtin, t_command *command, t_env **envp)
 	return (0);
 }
 
-//@TODO: handle heredoc signals not working yet
-int	pipex(t_env **envp, t_list **cmd_list)
-{
-	int		pipefd[2];
-	pid_t	cpid;
-	t_list	*tmp_list;
-	t_command *cmd;
-	int	size;
-	int	*pid_array;
-
-	size = ft_lstsize(*cmd_list);
-	pid_array = malloc(sizeof(int) * size);
-	pid_array[0] = size;
-	size = 1;
-	tmp_list = *cmd_list;
-	init_signals_when_children();
-	while (tmp_list->next)
-	{
-		if (handle_redirects(tmp_list->content, C_HERE_DOC | C_OPEN_INFILE))
-			return (EXIT_FAILURE);
-		if (pipe(pipefd) == -1)
-			return (rperror("pipe"));
-		cpid = fork();
-		if (cpid == -1)
-			return (rperror("fork"));
-		else if (cpid == 0 && g_signal == 0)
-		{
-			init_signals_noninteractive();
-			cmd = (t_command *)tmp_list->content;
-			if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-				exit(rperror("dup2"));
-			close(pipefd[1]);
-			if (handle_redirects(cmd, C_OPEN_OUT_TRUNC | C_OPEN_OUT_APP))
-				exit (EXIT_FAILURE);
-			if (cmd->flags & C_BUILTIN)
-				exit(exec_builtin(is_builtin(cmd->cmd[0]), cmd, envp));
-			else
-			{
-				char **envp_array = env_to_array(*envp);
-				make_exec(cmd, envp_array);
-			}
-			exit(errno);
-		}
-		init_signals_when_children();
-		close(pipefd[1]);
-		if (dup2(pipefd[0], STDIN_FILENO) == -1)
-			return (rperror("dup2"));
-		close(pipefd[0]);
-		tmp_list = tmp_list->next;
-		pid_array[size++] = cpid;
-	}
-	return (exec_to_stdout(envp, ft_lstlast(*cmd_list)->content, pid_array));
-}
-
 int	wait_for_children(int *chld_pids)
 {
 	int status;
@@ -120,47 +66,155 @@ int	wait_for_children(int *chld_pids)
 	return (status);
 }
 
-int	exec_to_stdout(t_env **envp, t_command *cmd, int *chld_pids)
+int has_flags(t_command *command, int wordmask_in_or_out)
 {
-	pid_t	cpid;
-	int		status;
-	char	**envp_array;
-	int		builtin_nb;
+	t_list		*redir_list;
+	t_redirect	*redir;
 
-	builtin_nb = is_builtin(cmd->cmd[0]);
-	if (chld_pids[0] == 1 && builtin_nb)
+	redir_list = command->redirects;
+	while (redir_list && redir_list->content && !g_signal)
 	{
-		if (handle_redirects(cmd, C_HERE_DOC | C_OPEN_INFILE | C_OPEN_OUT_TRUNC | C_OPEN_OUT_APP))
-			return(EXIT_FAILURE);
-		return (exec_builtin(builtin_nb, cmd, envp));
+		redir = redir_list->content;
+		if (redir->filename->flags & wordmask_in_or_out)
+			return (1);
+		redir_list = redir_list->next;
 	}
-	if (handle_redirects(cmd, C_HERE_DOC | C_OPEN_INFILE))
-		return (EXIT_FAILURE);
-	cpid = fork();
-	if (cpid == -1)
-	{
-		free(chld_pids);
-		return (rperror("fork"));
-	}
-	else if (cpid == 0)
-	{
-		init_signals_noninteractive();
-		if (handle_redirects(cmd, C_OPEN_OUT_TRUNC | C_OPEN_OUT_APP))
-			exit (EXIT_FAILURE);
-		if (builtin_nb)
-			exit(exec_builtin(builtin_nb, cmd, envp));
-		envp_array = env_to_array(*envp);
-		if (!envp_array)
-			exit(EXIT_FAILURE);
-		errno = make_exec(cmd, envp_array);
-		// perror("execve");
-		exit(errno);
-	}
-	waitpid(cpid, &status, 0);
-	wait_for_children(chld_pids);
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
-	if (WIFSIGNALED(status)) //probably remove this, cause i am doing this in main. exit with status in heredoc
-		return (128 + WTERMSIG(status)); // we might not even need the global now that i think about it, cause we do all the exit signal operations ins main
-	return (status);
+	return (0);
 }
+
+int exec_to_stdout(t_env **envp, t_command *cmd, int *chld_pids, int prev_fd)
+{
+    pid_t   cpid;
+    int     status;
+    char    **envp_array;
+    int     builtin_nb;
+
+    builtin_nb = is_builtin(cmd->cmd[0]);
+    if (chld_pids[0] == 1 && builtin_nb)
+    {
+        if (handle_redirects(cmd, C_HERE_DOC | C_OPEN_INFILE | C_OPEN_OUT_TRUNC | C_OPEN_OUT_APP))
+            return (EXIT_FAILURE);
+        return (exec_builtin(builtin_nb, cmd, envp));
+    }
+    if (handle_redirects(cmd, C_HERE_DOC | C_OPEN_INFILE))
+        return (EXIT_FAILURE);
+    cpid = fork();
+    if (cpid == -1)
+    {
+        free(chld_pids);
+        return (rperror("fork"));
+    }
+    else if (cpid == 0)
+    {
+        init_signals_noninteractive();
+
+        if (prev_fd != -1 && !(has_flags(cmd, C_HERE_DOC | C_OPEN_INFILE)))
+        {
+            if (dup2(prev_fd, STDIN_FILENO) == -1)
+                exit(rperror("dup2"));
+            close(prev_fd);
+        }
+
+        if (handle_redirects(cmd, C_OPEN_OUT_TRUNC | C_OPEN_OUT_APP))
+            exit(EXIT_FAILURE);
+        if (builtin_nb)
+            exit(exec_builtin(builtin_nb, cmd, envp));
+        envp_array = env_to_array(*envp);
+        if (!envp_array)
+            exit(EXIT_FAILURE);
+        errno = make_exec(cmd, envp_array);
+        exit(errno);
+    }
+    if (prev_fd != -1)
+        close(prev_fd);
+
+    waitpid(cpid, &status, 0);
+    wait_for_children(chld_pids);
+    if (WIFEXITED(status))
+        return (WEXITSTATUS(status));
+    if (WIFSIGNALED(status))
+        return (128 + WTERMSIG(status));
+    return (status);
+}
+
+int pipex(t_env **envp, t_list **cmd_list)
+{
+    int         pipefd[2];
+    pid_t       cpid;
+    t_list      *tmp_list;
+    t_command   *cmd;
+    int         size;
+    int         *pid_array;
+    int         prev_fd = -1;
+
+    size = ft_lstsize(*cmd_list);
+    pid_array = malloc(sizeof(int) * size);
+    if (!pid_array)
+        return (rperror("malloc"));
+    pid_array[0] = size;
+    size = 1;
+    tmp_list = *cmd_list;
+    init_signals_when_children();
+
+    while (tmp_list->next)
+    {
+	
+		if (pipe(pipefd) == -1)
+			return (rperror("pipe"));
+        if (handle_redirects(tmp_list->content, C_HERE_DOC | C_OPEN_INFILE))
+        {
+            close(pipefd[1]);
+			if (dup2(pipefd[0], STDIN_FILENO) == -1) {
+				perror("dup2");
+			}
+            close(pipefd[0]);
+            if (prev_fd != -1)
+            {
+                close(prev_fd);
+                prev_fd = -1;
+            }
+            tmp_list = tmp_list->next;
+            continue;
+        }
+
+        cpid = fork();
+        if (cpid == -1)
+            return (rperror("fork"));
+        else if (cpid == 0 && g_signal == 0)
+        {
+            init_signals_noninteractive();
+            cmd = (t_command *)tmp_list->content;
+            if (prev_fd != -1 && !(has_flags(cmd, C_HERE_DOC | C_OPEN_INFILE)))
+            {
+                if (dup2(prev_fd, STDIN_FILENO) == -1)
+                    exit(rperror("dup2"));
+                close(prev_fd);
+            }
+            if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+                exit(rperror("dup2"));
+            close(pipefd[1]);
+            close(pipefd[0]);
+            if (handle_redirects(cmd, C_OPEN_OUT_TRUNC | C_OPEN_OUT_APP))
+                exit(EXIT_FAILURE);
+            if (cmd->flags & C_BUILTIN)
+                exit(exec_builtin(is_builtin(cmd->cmd[0]), cmd, envp));
+            else
+            {
+                char **envp_array = env_to_array(*envp);
+                if (!envp_array)
+                    exit(EXIT_FAILURE);
+                make_exec(cmd, envp_array);
+            }
+            exit(errno);
+        }
+        close(pipefd[1]);
+        if (prev_fd != -1)
+            close(prev_fd);
+        prev_fd = pipefd[0];
+        init_signals_when_children();
+        tmp_list = tmp_list->next;
+        pid_array[size++] = cpid;
+    }
+    return (exec_to_stdout(envp, ft_lstlast(*cmd_list)->content, pid_array, prev_fd));
+}
+
