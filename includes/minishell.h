06cbbf7e58a82c21ab4e4ab47243e4b9084611b6
Steven Nkeneng <47323934+nkeneng@users.noreply.github.com>
2024-12-19 16:36:03 +0100

    refactor pipex and exec_to_stdout functions for improved signal handl… (#66)
    * refactor pipex and exec_to_stdout functions for improved signal handling and code clarity
    
    * remove exec_to_stdout function declaration from minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -72,7 +72,6 @@ int					make_exec(t_command *command, char *envp[]);
 
 // command.c
 int					exec_command(t_command *command, t_env **envp, int *fd);
-int					exec_to_stdout(t_env **envp, t_command *cmd, int *chld_pids);
 int					pipex(t_env **envp, t_list **cmd_list);
 
 // init_envp.c
