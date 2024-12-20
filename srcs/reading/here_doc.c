7b1c267c7762cd6d7cd99d0d11cdaace1d20ebdb
Lea Meubrink <lmeubrin@student.42berlin.de>
2024-12-20 15:10:12 +0100

    expansion in HD leakfree and normed

--- srcs/reading/here_doc.c
+++ srcs/reading/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 08:24:07 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/12/20 14:44:51 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/12/20 15:09:06 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,42 @@
 char	*expand_line(t_shell *shell, char *line)
 {
 	t_word_desc	*container;
+	char		*new_line;
 
 	container = wd_make_word(line, ft_strlen(line), 0);
 	if (!container)
 		return (NULL);
 	while (contains_more_vars(container))
 		container = wd_expand_var(&container, shell);
-	line = container->word;
+	new_line = container->word;
 	free(container);
-	return (line);
+	free(line);
+	return (new_line);
+}
+
+//checkts if line is delimiter and frees line
+//if this returns 1, line was the delimiter, should return from heredoc
+int	line_is_delim(char *line, char *delim)
+{
+	int		delim_len;
+
+	delim_len = ft_strlen(delim);
+	if (!ft_strncmp(delim, line, delim_len) && \
+		(line[delim_len] == '\n' || line[delim_len] == '\0'))
+	{
+		free(line);
+		line = get_next_line(-1);
+		return (1);
+	}
+	return (0);
 }
 
 //does heredoc
 int	here_doc(t_shell *shell, char *delim, int flags)
 {
 	char	*line;
-	int		delim_len;
-	int		expand;
 
-	delim_len = ft_strlen(delim);
 	reopen_stdin();
-	expand = !(flags & (W_DQUOTED | W_SQUOTED));
 	while (1)
 	{
 		if (g_signal == SIGINT)
@@ -47,15 +62,9 @@ int	here_doc(t_shell *shell, char *delim, int flags)
 			break ;
 		if (g_signal == SIGINT)
 			return (130);
-		if (!ft_strncmp(delim, line, delim_len) && \
-			(line[delim_len] == '\n' || line[delim_len] == '\0'))
-		{
-			free(line);
-			line = get_next_line(-1);
+		if (line_is_delim(line, delim))
 			break ;
-		}
-		(void) shell;
-		if (expand)
+		if (!(flags & (W_DQUOTED | W_SQUOTED)))
 			line = expand_line(shell, line);
 		if (!line)
 			return (rperror("malloc"));
