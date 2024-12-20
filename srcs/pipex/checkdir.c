cbd9ac059f869c74e23dbdbb7016219b3d54b9e3
Lea Meubrink <lmeubrin@student.42berlin.de>
2024-12-20 12:31:55 +0100

    file checkdir with checkdir funciton added

---
+++ srcs/pipex/checkdir.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkdir.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 19:40:29 by lmeubrin          #+#    #+#             */
+/*   Updated: 2024/12/19 20:05:46 by lmeubrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+// check if the path is a directory. return 0 if not a dir, 126 if dir
+int	checkdir(char *path)
+{
+	struct stat	buffer;
+
+	if (stat(path, &buffer) == -1)
+	{
+		perror("stat");
+		return (EXIT_FAILURE);
+	}
+	if (S_ISDIR(buffer.st_mode))
+	{
+		ft_fprintf(2, "%s: is a directory\n", path);
+		return (126);
+	}
+	return (0);
+}
