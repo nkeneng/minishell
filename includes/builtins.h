388da0b4ed77e868999367a167d4ab723e3dcb49
steven nkeneng <nkeneng.junior@yahoo.fr>
2024-12-24 11:19:25 +0000

    Refactor built-in commands to include environment variable handling in ft_cd and update function signatures

--- includes/builtins.h
+++ includes/builtins.h
@@ -15,11 +15,12 @@
 
 # include "minishell.h"
 
-int		ft_cd(char **cmd);
+int	ft_export(char **args, t_env **envp);
+void	ft_override_env(char *key, char *value, t_env **envp);
+int		ft_cd(char **cmd, t_env **envp);
 int	ft_unset(char **args, t_env **envp);
 int     ft_echo(char **args);
 int	ft_exit(char **cmd);
 int	ft_pwd(void);
 int	ft_env(char **envp);
-int	ft_export(char **args, t_env **envp);
 #endif
--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -12,10 +12,22 @@
 
 #include "../../includes/minishell.h"
 
-// TODO: update the PWD environment variable
-// TODO: update the OLDPWD environment variable before changing cwd
-// TODO: handle previous directory ( - )
-int	ft_cd(char **cmd)
+static void update_pwd_vars(t_env **envp)
+{
+    char *cwd;
+    char *pwd;
+
+    cwd = getcwd(NULL, 0);
+    if (cwd)
+    {
+		pwd = envp_keytovalue("PWD", *envp, ft_strlen("PWD"));
+		ft_override_env("OLDPWD", pwd, envp);
+		ft_override_env("PWD", cwd, envp);
+        free(cwd);
+    }
+}
+
+int	ft_cd(char **cmd, t_env **envp)
 {
 	char	*home;
 
@@ -32,12 +44,59 @@ int	ft_cd(char **cmd)
 			perror("HOME not set");
 			return (EXIT_FAILURE);
 		}
-		chdir(getenv("HOME"));
+		if (chdir(home) == -1)
+		{
+			perror("chdir");
+			return (EXIT_FAILURE);
+		}
 	}
-	else if (chdir(cmd[1]) == -1)
-	{
-		perror("chdir");
-		return (EXIT_FAILURE);
-	}
+    else if (cmd && cmd[1])
+    {
+        if (!ft_strncmp(cmd[1], "-", 1))
+        {
+            char *oldpwd = envp_keytovalue("OLDPWD", *envp, ft_strlen("OLDPWD"));
+            if (!oldpwd)
+            {
+                ft_fprintf(2, "cd: OLDPWD not set\n");
+                return (EXIT_FAILURE);
+            }
+            if (chdir(oldpwd) == -1)
+            {
+                perror("chdir");
+                return (EXIT_FAILURE);
+            }
+        }
+        else if (cmd[1][0] == '~')
+        {
+            char *home = getenv("HOME");
+            if (!home)
+            {
+                ft_fprintf(2, "cd: HOME not set\n");
+                return (EXIT_FAILURE);
+            }
+            if (cmd[1][1])
+            {
+                char *path = ft_strjoin(home, cmd[1] + 1);
+                if (chdir(path) == -1)
+                {
+                    perror("chdir");
+                    free(path);
+                    return (EXIT_FAILURE);
+                }
+                free(path);
+            }
+            else if (chdir(home) == -1)
+            {
+                perror("chdir");
+                return (EXIT_FAILURE);
+            }
+        }
+        else if (chdir(cmd[1]) == -1)
+        {
+            perror("chdir");
+            return (EXIT_FAILURE);
+        }
+    }
+	update_pwd_vars(envp);
 	return (EXIT_SUCCESS);
 }
