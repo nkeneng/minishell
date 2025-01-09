046e9c6bec84ea72ee434ec6ccd9d05566223fc2
Moat423 <schluesselfehlt@gmail.com>
2025-01-09 10:44:00 +0100

    perilious extracting of function for norme

--- srcs/parser/split_at_space.c
+++ srcs/parser/split_at_space.c
@@ -6,33 +6,28 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:07:48 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/12/17 18:10:14 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/01/09 10:42:45 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-t_word_list	*split_at_spaces(t_word_list *item)
+t_word_list	*helper_split(t_word_list *item, int sep, t_word_list *curr)
 {
 	t_word_desc	*new_item;
-	t_word_list	*curr;
 	int			i;
 	int			begin_word;
-	int			s;
 
-	if (item->word->flags & W_SQUOTED || item->word->flags & W_DQUOTED)
-		return (NULL);
-	s = ft_whitespace_seperator(&item->word->word[0]);
-	i = s;
+	i = sep;
 	begin_word = i;
-	curr = item;
 	while (item->word->word[i])
 	{
-		s = ft_whitespace_seperator(&item->word->word[i]);
-		if (s)
+		sep = ft_whitespace_seperator(&item->word->word[i]);
+		if (sep)
 		{
-			i += s;
-			new_item = wd_make_word(&item->word->word[begin_word], i - begin_word, item->word->flags);
+			i += sep;
+			new_item = wd_make_word(&item->word->word[begin_word], i
+					- begin_word, item->word->flags);
 			curr = wl_insert(curr, new_item);
 			if (!curr)
 				return (NULL);
@@ -43,26 +38,30 @@ t_word_list	*split_at_spaces(t_word_list *item)
 	return (curr);
 }
 
-// takes a line and splits the line at whitespaces and puts them 
-// into word_desc items and concats them into a word_list
-t_word_list	*word_list_ft_split(char *line, int flags)
+t_word_list	*split_at_spaces(t_word_list *item)
 {
-	int			i;
+	int	s;
+
+	if (item->word->flags & W_SQUOTED || item->word->flags & W_DQUOTED)
+		return (NULL);
+	s = ft_whitespace_seperator(&item->word->word[0]);
+	return (helper_split(item, s, item));
+}
+
+int	seperate_words_in_line(char *line, int flags, int i, t_word_list **head)
+{
 	int			begin_word;
 	int			s;
-	t_word_list	*head;
 
-	i = ft_skip_whitespace(line);
 	begin_word = i;
-	head = NULL;
 	while (line[i])
 	{
 		s = ft_whitespace_seperator(&line[i]);
 		if (s)
 		{
-			if (!wl_add_node(&head, &line[begin_word], \
-					i - begin_word, flags | W_SPLITSPACE))
-				return (NULL);
+			if (!wl_add_node(head, &line[begin_word], i - begin_word,
+					flags | W_SPLITSPACE))
+				return (-1);
 			i += s;
 			begin_word = i;
 			continue ;
@@ -70,6 +69,23 @@ t_word_list	*word_list_ft_split(char *line, int flags)
 		else
 			i++;
 	}
+	return (begin_word);
+}
+
+// takes a line and splits the line at whitespaces and puts them
+// into word_desc items and concats them into a word_list
+t_word_list	*word_list_ft_split(char *line, int flags)
+{
+	int			i;
+	int			begin_word;
+	t_word_list	*head;
+
+	i = ft_skip_whitespace(line);
+	head = NULL;
+	begin_word = seperate_words_in_line(line, flags, i, &head);
+	if (begin_word == -1)
+		return (NULL);
+	i = ft_strlen(line);
 	if (begin_word != i)
 		if (!wl_add_node(&head, &line[begin_word], i - begin_word, flags))
 			return (NULL);
