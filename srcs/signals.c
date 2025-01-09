d6c966d772d6beaed9a58237a7c6f058b5021584
Moat423 <schluesselfehlt@gmail.com>
2025-01-09 08:05:43 +0100

    moved heredoc_signals from deleted file to signals

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,12 +6,27 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:19:29 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/01/07 14:29:41 by admin            ###   ########.fr       */
+/*   Updated: 2025/01/09 07:56:05 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <unistd.h>
+
+// ignore SIGQUIT and SIGINT
+void	init_signals_heredoc(void)
+{
+	struct sigaction	sa;
+
+	ft_memset(&sa, 0, sizeof(sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = SIG_IGN;
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
+		perror("Error: cannot handle SIGQUIT");
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		perror("Error: cannot handle SIGINT");
+}
 
 void	signal_handler_when_children(int signum)
 {
