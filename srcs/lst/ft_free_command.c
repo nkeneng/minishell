147b2e6ad4d0034445dd604e348a09bec639d725
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-01-19 13:03:49 +0100

    changed ft_free_word_desc to ft_free_redirect

--- srcs/lst/ft_free_command.c
+++ srcs/lst/ft_free_command.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:39:22 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/11/27 14:53:37 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/01/19 12:55:33 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_free_command(void *command)
 	t_command	*com;
 
 	com = (t_command *) command;
-	ft_lstclear(&(com->redirects), ft_free_word_desc);
+	ft_lstclear(&(com->redirects), ft_free_redirect);
 	free_char_array(com->cmd, 0);
 	free(com);
 	return ;
